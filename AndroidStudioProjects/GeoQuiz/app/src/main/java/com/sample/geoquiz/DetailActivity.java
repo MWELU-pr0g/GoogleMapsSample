972be24a9d8e377631f759b5a5082b0c2a31c7f7
MWELU MUTINDA <carol.mweluh@gmail.com>
2020-01-20 10:35:34 +0300

    listview

---
+++ AndroidStudioProjects/GeoQuiz/app/src/main/java/com/sample/geoquiz/DetailActivity.java
@@ -0,0 +1,34 @@
+package com.sample.geoquiz;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        String country = getIntent().getStringExtra("country");
+
+        textView = findViewById(R.id.label);
+        textView.setText(country);
+
+    }
+
+}
--- AndroidStudioProjects/GeoQuiz/app/src/main/java/com/sample/geoquiz/MainActivity.java
+++ AndroidStudioProjects/GeoQuiz/app/src/main/java/com/sample/geoquiz/MainActivity.java
@@ -2,13 +2,58 @@ package com.sample.geoquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private  ArrayAdapter arrayAdapter;
+
+    private String[] countries;
+
+    private ListView listView;
+//    private String TAG = "Mainactivity";
+    private static final String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Resources resources = getResources();
+        countries = resources.getStringArray(R.array.countiesName);
+
+        arrayAdapter = new ArrayAdapter(this, R.layout.list_view, countries);
+        listView = findViewById(R.id.list1);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.e(TAG, "onItemClick: "+ position);
+
+                String country = countries[position];
+
+                goToDetail(country);
+
+
+            }
+        });
+
+
+    }
+
+    private void goToDetail(String country) {
+
+        Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+        intent.putExtra("country", country);
+        startActivity(intent);
     }
 }
